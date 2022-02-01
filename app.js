9ed086d81985440d8d31477919ad177431ae7e35
Aniket Gajanan Panchal <aniketpanchal52648@gmail.com>
2022-02-01 18:31:07 +0530

    first commit after pull

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 if(process.env.NODE_ENV !=='production'){
     require('dotenv').config();
 }
+
+
+
 console.log(process.env.cloud_name);
 const express = require('express');
 const mongoose = require('mongoose');
